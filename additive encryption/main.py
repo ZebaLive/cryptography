86c9300f05439ed38149be1d90defbadfd0412cf
Paulius <paulius.pluta@gmail.com>
2021-09-27 12:40:19 +0300

    Added Affine cypher

--- additive encryption/main.py
+++ additive encryption/main.py
@@ -4,40 +4,54 @@ import multiplicative_crypter
 
 print("Additive encryption")
 print("-------------------")
-cypher = additive_crypter.AdditiveCrypter()
+cypher_additive = additive_crypter.AdditiveCrypter()
+cypher_multiplicative = multiplicative_crypter.MultiplicativeCrypter()
 
 print("Please select one")
 selector = 0
-while selector < 3:
-    selector = int(input("[1] additive encryption, [2] multiplicative encryption, [3] exit: "))
+while selector < 4:
+    selector = int(input("[1] additive encryption, [2] multiplicative encryption, [3] affine, [4] exit: "))
     if selector == 1:
-        cypher = additive_crypter.AdditiveCrypter()
         if int(input("[1] encryption, [2] decryption: ")) == 1:
             text = input("Input raw [text]: ")
             key = input("Input key [number]: ")
             print("Encrypting text...")
             print("Encypted text:")
-            print(cypher.encryption(text, key))
+            print(cypher_additive.encryption(text, key))
         else:
             text = input("Input encrypted [text]: ")
             key = input("Input key [number]: ")
             print("Decrypting text...")
             print("Raw text:")
-            print(cypher.decryption(text, key))
+            print(cypher_additive.decryption(text, key))
 
-    if selector == 2 :
-        cypher = multiplicative_crypter.MultiplicativeCrypter()
+    if selector == 2:
         if int(input("[1] encryption, [2] decryption: ")) == 1:
             text = input("Input raw [text]: ")
             key = input("Input key [number]: ")
             print("Encrypting text...")
             print("Encypted text:")
-            print(cypher.encryption(text, key))
+            print(cypher_multiplicative.encryption(text, key))
         else:
             text = input("Input encrypted [text]: ")
             key = input("Input key [number]: ")
             print("Decrypting text...")
             print("Raw text:")
-            print(cypher.decryption(text, key))
+            print(cypher_multiplicative.decryption(text, key))
 
+    if selector == 3:
+        if int(input("[1] encryption, [2] decryption: ")) == 1:
+            text = input("Input raw [text]: ")
+            key1 = input("Input key one [number]: ")
+            key2 = input("Input key two [number]: ")
+            print("Encrypting text...")
+            print("Encypted text:")
+            print(cypher_additive.encryption(cypher_multiplicative.encryption(text, key2), key1))
+        else:
+            text = input("Input encrypted [text]: ")
+            key1 = input("Input key one [number]: ")
+            key2 = input("Input key two [number]: ")
+            print("Decrypting text...")
+            print("Raw text:")
+            print(cypher_multiplicative.decryption(cypher_additive.decryption(text, key1), key2))
     
