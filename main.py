8fbdc58335bce826aabe7b5c24339db476d5069d
Paulius <paulius.pluta@gmail.com>
2021-10-04 15:55:03 +0300

    updated user interface

--- main.py
+++ main.py
@@ -1,6 +1,155 @@
 #main application interface
-import additive, multiplicative, autokey, keyed
+import os, additive, multiplicative, autokey, keyed 
 
+class ui:
+    def __init__(self):
+        self.cypher = {
+            "additive" : additive.AdditiveCrypter(),
+            "multiplicative" : multiplicative.MultiplicativeCrypter(),
+            "autokey" : autokey.AdditiveCrypter()
+        }
+
+    def main_meniu(self):
+        os.system('cls||clear')
+        print("---Mian Meniu---")
+        print ("""
+        1.Additive Cypher
+        2.Multiplicative Cypher
+        3.Affine Cypher
+        4.Autokey Cypher
+        5.Keyed Transposition Cypher
+        6.Exit
+        """)
+        
+        selector = input("Selection: ") 
+        if selector == "1":
+            self.additive()
+        elif selector == "2":
+            self.multiplicative()
+        elif selector == "3":
+            self.affine()
+        elif selector == "4":
+            self.autokey()
+        elif selector == "5":
+            self.keyed()
+        elif selector == "6":
+            self.exit()
+            return
+        self.main_meniu()
+        return
+
+    def exit(self):
+        os.system('cls||clear')
+        print ("""
+        Thank you for using my cypher program and have a good day!
+        
+        Copyright© 2021 Paulius Pluta
+        """)
+
+    def additive(self):
+        os.system('cls||clear')
+        print("---Additive Cypher---")
+        print("""
+        1. Encryption
+        2. Decryption
+        3. Exit
+        """)
+        selector = input("Selection: ") 
+        if selector == "1":
+            os.system('cls||clear')
+            text = input("Input raw text: ")
+            key = input("Input key (number): ")
+            print("Encrypting text...")
+            print("Encypted text:")
+            print(self.cypher["additive"].encryption(text, key))
+        elif selector == "2":
+            os.system('cls||clear')
+            text = input("Input encrypted text: ")
+            key = input("Input key (number): ")
+            print("Decrypting text...")
+            print("Raw text:")
+            print(self.cypher["additive"].decryption(text, key))
+        elif selector == "3":
+            return
+        else:
+            self.additive()
+            return
+        input("Press anything to go back to main meniu...")
+        return
+    
+    def multiplicative(self):
+        os.system('cls||clear')
+        print("---Multiplicative Cypher---")
+        print("""
+        1. Encryption
+        2. Decryption
+        3. Exit
+        """)
+        selector = input("Selection: ") 
+        if selector == "1":
+            os.system('cls||clear')
+            text = input("Input raw text: ")
+            key = input("Input key (number): ")
+            print("Encrypting text...")
+            print("Encypted text:")
+            print(self.cypher["multiplicative"].encryption(text, key))
+        elif selector == "2":
+            os.system('cls||clear')
+            text = input("Input encrypted text: ")
+            key = input("Input key (number): ")
+            print("Decrypting text...")
+            print("Raw text:")
+            print(self.cypher["multiplicative"].decryption(text, key))
+        elif selector == "3":
+            return
+        else:
+            self.additive()
+            return
+        input("Press anything to go back to main meniu...")
+        return
+
+    def affine(self):
+        os.system('cls||clear')
+        print("---Multiplicative Cypher---")
+        print("""
+        1. Encryption
+        2. Decryption
+        3. Exit
+        """)
+        selector = input("Selection: ") 
+        if selector == "1":
+            os.system('cls||clear')
+            text = input("Input raw text: ")
+            key = input("Input key (number): ")
+            print("Encrypting text...")
+            print("Encypted text:")
+            print(self.cypher["multiplicative"].encryption(text, key))
+        elif selector == "2":
+            os.system('cls||clear')
+            text = input("Input encrypted text: ")
+            key = input("Input key (number): ")
+            print("Decrypting text...")
+            print("Raw text:")
+            print(self.cypher["multiplicative"].decryption(text, key))
+        elif selector == "3":
+            return
+        else:
+            self.additive()
+            return
+        input("Press anything to go back to main meniu...")
+        return
+
+    def autokey(self):
+        pass
+
+    def keyed(self):
+        pass
+
+
+user_interface = ui()   
+
+user_interface.main_meniu()
+"""
 print("Additive encryption")
 print("-------------------")
 cypher_additive = additive.AdditiveCrypter()
@@ -76,3 +225,4 @@ while selector < 5:
             print("Decrypting text...")
             print("Raw text:")
             print(cypher_autokey.decryption(text, key))
+"""
